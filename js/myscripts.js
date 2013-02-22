7cb414001c18c5a984a9a8476bf54c7d7b7b9a4f
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-02-21 23:16:19 -0800

    Ajax navagation
    * The Ajax navagation works.
    * Super basic styling. only to tell what is what

---
+++ js/myscripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	document.addEventListener('touchmove', function(e){ e.preventDefault(); }, false);
+	
+	
+	
+	$('nav ul li a').on('touchstart', function(){
+		changePage($(this).attr('data-file'));
+		});
+		
+	$('nav ul li:first-of-type a').trigger('touchstart');
+	
+
+
+});
+
+function changePage(fileName){
+	$('.content_container').animate({opacity:0}, 500, function(){
+		// load content
+		$('.content_loading_container').load('pages/'+fileName, function(){
+			$('.content_container').delay(250).animate({opacity:1}, 500);
+		});
+	});
+}
