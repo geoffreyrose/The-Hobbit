e4e79e6cfc442caf3a658a50d7f5381a48218d45
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-02-22 00:22:19 -0800

    nav styling
    * styled nav
    * icon fonts
    * web fonts

---
+++ fonts/icomoon/lte-ie7.js
@@ -0,0 +1,32 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-movie' : '&#xe000;',
+			'icon-image' : '&#xe001;',
+			'icon-accessibility' : '&#xe002;',
+			'icon-user' : '&#xe003;',
+			'icon-home' : '&#xe004;',
+			'icon-file' : '&#xe005;',
+			'icon-facebook' : '&#xe006;',
+			'icon-twitter' : '&#xe007;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
