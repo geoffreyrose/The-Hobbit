c7654644117cdce4c22afd9972181f9c6054c725
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2013-02-27 12:23:30 -0800

    not ipad site is RWD
    * finished adding all the content to the non ipad web app site

--- notipad/js/myscripts.js
+++ notipad/js/myscripts.js
@@ -1,48 +1,7 @@
-var myScroll;
-function loaded() {
-	myScroll = new iScroll('scroll',{checkDOMChanges:true});
-}
-
-var isiPad = navigator.userAgent.indexOf('iPad') != -1;
-
-document.addEventListener('DOMContentLoaded', loaded, false);
-
 $(document).ready(function(){
-
-
-
-	document.addEventListener('touchmove', function(e){ e.preventDefault(); }, false);
+	$("header").addClass("mobileheader");
 	
-	$('nav ul li a').on('touchstart', function(){
-		changePage($(this).attr('data-file'));
-		});
-	
-		
-	$('nav ul li:first-of-type a').trigger('touchstart');
-	
-	checkDevice();
-	checkiPadStandAlone();
-
-});
-function checkDevice(){
-	if(window.isiPad){
-		// do nothing
-	}else{
-		window.location = "notipad/index.html";
-	}
-}
-
-function checkiPadStandAlone(){
-	if(window.navigator.standalone == false) {
-		$('.container').css('display','none');
-	}
-}
-
-function changePage(fileName){
-	$('.content_container').animate({opacity:0}, 500, function(){
-		// load content
-		$('.content_loading_container').load('pages/'+fileName, function(){
-			$('.content_container').delay(250).animate({opacity:1}, 500);
-		});
+	$('#mobilenav').click(function() {
+		$('header').toggleClass("mobileheader");
 	});
-}
+});
